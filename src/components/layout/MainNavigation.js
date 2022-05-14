349a345a4b647d6c1d0014f89c8a5c1a4dc1e07d
jtfreedman <babzibles@gmail.com>
2022-05-14 13:59:13 -0400

    fixing nav bar for additional top bar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -16,22 +16,23 @@ function MainNavigation()
 
     return (
         <header className={classes.header}>
-            <div className={classes.logo} onClick={routeChange}>myWatches</div>
             <nav>
-              <ul>
-                <li>
-                    <Link to='/all-watches'>All Watches</Link>
-                </li>
-                <li>
-                    <Link to='/new-watch'>Add New Watch</Link>
-                </li>
-                <li>
-                    <Link to='/favorites'>
-                        My Favorites 
-                        <span className={classes.badge}>{favContext.total}</span>
-                    </Link>
-                </li>
-              </ul>  
+                
+                <ul>
+                    <div className={classes.logo} onClick={routeChange}>myWatches</div>
+                    <li>
+                        <Link to='/all-watches'>All Watches</Link>
+                    </li>
+                    <li>
+                        <Link to='/new-watch'>Add New Watch</Link>
+                    </li>
+                    <li>
+                        <Link to='/favorites'>
+                            My Favorites 
+                            <span className={classes.badge}>{favContext.total}</span>
+                        </Link>
+                    </li>
+                </ul>  
             </nav>
         </header>
     );

1ba9dc2fed631172331f3b1f06d24265ae156810
jtfreedman <babzibles@gmail.com>
2022-05-15 14:51:22 -0400

    loginHeader now in layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,11 @@
+import LoginHeader from '../ui/LoginHeader';
 import classes from './Layout.module.css';
 import MainNavigation from './MainNavigation';
 
 function Layout(props) {
     return (
         <div>
+            <LoginHeader />
             <MainNavigation />
             <main className={classes.main}>
                 {props.children}

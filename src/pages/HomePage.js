1eeff0c2c541c95c2c5123c91282755e4724bd8a
jtfreedman <babzibles@gmail.com>
2022-05-15 12:33:09 -0400

    home page improvements

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,16 @@ import MainNavigation from '../components/layout/MainNavigation';
 function HomePage() {
     return (
         <>
+            <div className={classes.desktop}>
+                <span className={classes.desktop__shipping}>Free U.S. Standard Shipping Orders $50+</span>
+                <span className={classes.desktop__about}>About Us</span>
+                <span className={classes.desktop__locations}>Locations</span>
+                <span className={classes.desktop__track}>Track Order</span>
+                <span className={classes.desktop__login}>Log In</span>
+            </div>
+
             <MainNavigation />
+
             <header className={classes.image}>
                 <div>Login</div>
             </header>

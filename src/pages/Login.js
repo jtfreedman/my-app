d83b957e3349a9b1e4c994bb4cb6508a1d052fc2
jtfreedman <babzibles@gmail.com>
2022-05-15 21:40:10 -0400

    more added to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,20 @@
 import classes from './Login.module.css'
 
 function Login() {
+
+    function submitHandler(event) {}
+
+
     return (
         <div className={classes.loginForm}>
             <div className={classes.loginButton}>Login</div>
             <div className={classes.createAccount}>Create Account</div>
+            <form className={classes.form} onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <input className={classes.loginText} type="text" required id="user"/>
+                    <input className={classes.loginPassword} type="text" required id="password"/>
+                </div>
+            </form>
         </div>
     );
 }

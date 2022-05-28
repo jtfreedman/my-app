618c648ffdf6b7f62841bb4b5d2464580ce8b904
jtfreedman <babzibles@gmail.com>
2022-05-28 06:53:22 -0400

    reverse single state to slices - try to add promo

--- src/components/users/NewUserForm.js
+++ src/components/users/NewUserForm.js
@@ -3,42 +3,53 @@ import { useRef, useState } from 'react';
 import classes from './NewUserForm.module.css';
 
 export default function NewUserForm(props) {
-    const [userInput, setUserInput] = useState({
-        currentFirstName: '',
-        currentLastName: '',
-        currentEmail: '',
-        currentPassword: '',
-        currentPasswordConfirm: ''
-    })
+    // const [userInput, setUserInput] = useState({
+    //     currentFirstName: '',
+    //     currentLastName: '',
+    //     currentEmail: '',
+    //     currentPassword: '',
+    //     currentPasswordConfirm: ''
+    // })
+
+    const [currentFirstName, setFirstName] = useState('');
+    const [currentLastName, setLastName] = useState('');
+    const [currentEmail, setEmail] = useState('');
+    const [currentPassword, setPassword] = useState('');
+    const [currentPasswordConfirm, setPasswordConfirm] = useState('');
 
     function firstNameChangeHandler(event) {
-        setUserInput((prevState) => {
-            return { ...prevState, currentFirstName: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, currentFirstName: event.target.value };
+        // });
+        setFirstName(event.target.value);
     };
 
     function lastNameChangeHandler(event) {
-        setUserInput((prevState) => {
-            return { ...prevState, currentLastName: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, currentLastName: event.target.value };
+        // });
+        setLastName(event.target.value);
     };
 
     function emailChangeHandler(event) {
-        setUserInput((prevState) => {
-            return { ...prevState, currentEmailName: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, currentEmailName: event.target.value };
+        // });
+        setEmail(event.target.value);
     };
 
     function passwordChangeHandler(event) {
-        setUserInput((prevState) => {
-            return { ...prevState, currentPassword: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, currentPassword: event.target.value };
+        // });
+        setPassword(event.target.value);
     };
 
     function passwordConfirmChangeHandler(event) {
-        setUserInput((prevState) => {
-            return { ...prevState, currentPasswordConfirm: event.target.value };
-        });
+        // setUserInput((prevState) => {
+        //     return { ...prevState, currentPasswordConfirm: event.target.value };
+        // });
+        setPasswordConfirm(event.target.value);
     };
 
     const firstNameInputRef = useRef();
@@ -46,7 +57,7 @@ export default function NewUserForm(props) {
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
     const passwordConfirmInputRef = useRef();
-    //const promotionalInputRef = useRef();
+    const promotionalInputRef = useRef();
 
     function submitHandler(event) {
         event.preventDefault();
@@ -56,12 +67,14 @@ export default function NewUserForm(props) {
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
         const enteredPasswordConfirm = passwordConfirmInputRef.current.value;
+        const enteredPromoOption = promotionalInputRef.current.value;
         
         const userData = {
             firstName: enteredFirstName,
             lastName: enteredLastName,
             email: enteredEmail,
-            password: enteredPassword
+            password: enteredPassword,
+            promo: enteredPromoOption
         };
 
         if (enteredPassword.normalize() === enteredPasswordConfirm.normalize()) {
@@ -87,7 +100,7 @@ export default function NewUserForm(props) {
                 <input className={classes.loginPassword} type="text" required id="passwordConfirm" placeholder="Confirm Password" ref={passwordConfirmInputRef} onChange={passwordConfirmChangeHandler}/>
             </div>
             <label className={classes.checkboxContainer}>
-                <input className={classes.checkmark} type="checkbox" name="checkbox" value="remember" />
+                <input className={classes.checkmark} type="checkbox" name="checkbox" value="remember" ref={promotionalInputRef}/>
                 <span ></span>
                 <div className={classes.checkmarkText}>I want to recieve promotional emails.</div>
             </label>

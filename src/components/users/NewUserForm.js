e4ea35728df7a12105c6cf85a1cdb629e0d338bd
jtfreedman <babzibles@gmail.com>
2022-05-28 06:21:27 -0400

    added single state, basic password check

--- src/components/users/NewUserForm.js
+++ src/components/users/NewUserForm.js
@@ -3,31 +3,54 @@ import { useRef, useState } from 'react';
 import classes from './NewUserForm.module.css';
 
 export default function NewUserForm(props) {
-    const [currentFirstName, setCurrentFirstName] = useState('');
-    const [currentLastName, setCurrentLastName] = useState('');
-    const [currentEmail, setCurrentEmail] = useState('');
-    const [currentPassword, setCurrentPassword] = useState('');
+    const [userInput, setUserInput] = useState({
+        currentFirstName: '',
+        currentLastName: '',
+        currentEmail: '',
+        currentPassword: '',
+        currentPasswordConfirm: ''
+    })
 
     function firstNameChangeHandler(event) {
-        setCurrentFirstName(event.target.value);
-    }
+        setUserInput({
+            ...userInput,
+            currentFirstName: event.target.value,
+        })
+    };
 
     function lastNameChangeHandler(event) {
-        setCurrentLastName(event.target.value);
-    }
+        setUserInput({
+            ...userInput,
+            currentLastName: event.target.value,
+        })
+    };
 
     function emailChangeHandler(event) {
-        setCurrentEmail(event.target.value);
-    }
+        setUserInput({
+            ...userInput,
+            currentEmailName: event.target.value,
+        })
+    };
 
-    function passwordNameChangeHandler(event) {
-        setCurrentPassword(event.target.value);
-    }
+    function passwordChangeHandler(event) {
+        setUserInput({
+            ...userInput,
+            currentPassword: event.target.value,
+        })
+    };
+
+    function passwordConfirmChangeHandler(event) {
+        setUserInput({
+            ...userInput,
+            currentPasswordConfirm: event.target.value,
+        })
+    };
 
     const firstNameInputRef = useRef();
     const lastNameInputRef = useRef();
     const emailInputRef = useRef();
     const passwordInputRef = useRef();
+    const passwordConfirmInputRef = useRef();
     //const promotionalInputRef = useRef();
 
     function submitHandler(event) {
@@ -37,7 +60,8 @@ export default function NewUserForm(props) {
         const enteredLastName = lastNameInputRef.current.value;
         const enteredEmail = emailInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
-
+        const enteredPasswordConfirm = passwordConfirmInputRef.current.value;
+        
         const userData = {
             firstName: enteredFirstName,
             lastName: enteredLastName,
@@ -45,8 +69,14 @@ export default function NewUserForm(props) {
             password: enteredPassword
         };
 
-        props.onAddUser(userData);
-    }
+        if (enteredPassword.normalize() === enteredPasswordConfirm.normalize()) {
+            props.onAddUser(userData);
+        }
+        else {
+            //do something
+        }
+        
+    };
 
     return (
         <form className={classes.form} onSubmit={submitHandler}>
@@ -57,9 +87,9 @@ export default function NewUserForm(props) {
                 
                 <input className={classes.loginText} type="text" required id="email" placeholder="Email"  ref={emailInputRef} onChange={emailChangeHandler}/>
 
-                <input className={classes.loginPassword} type="text" required id="password" placeholder="Password"  ref={passwordInputRef} onChange={passwordNameChangeHandler}/>
+                <input className={classes.loginPassword} type="text" required id="password" placeholder="Password"  ref={passwordInputRef} onChange={passwordChangeHandler}/>
 
-                <input className={classes.loginPassword} type="text" required id="passwordConfirm" placeholder="Confirm Password"/>
+                <input className={classes.loginPassword} type="text" required id="passwordConfirm" placeholder="Confirm Password" ref={passwordConfirmInputRef} onChange={passwordConfirmChangeHandler}/>
             </div>
             <label className={classes.checkboxContainer}>
                 <input className={classes.checkmark} type="checkbox" name="checkbox" value="remember" />

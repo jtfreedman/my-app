60b05904afcf7a300df9c88e49f4d87ccb3b38af
jtfreedman <babzibles@gmail.com>
2022-05-28 07:28:12 -0400

    state resets values after submission

--- src/components/users/NewUserForm.js
+++ src/components/users/NewUserForm.js
@@ -46,20 +46,56 @@ export default function NewUserForm(props) {
             //do something
         }
         
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+        setPasswordConfirm('');
     };
 
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
-                <input className={classes.loginText} type="text" required id="firstName" placeholder="First Name" onChange={firstNameChangeHandler}/>
-
-                <input className={classes.loginText} type="text" required id="lastName" placeholder="Last Name" onChange={lastNameChangeHandler}/>
-                
-                <input className={classes.loginText} type="text" required id="email" placeholder="Email" onChange={emailChangeHandler}/>
-
-                <input className={classes.loginPassword} type="text" required id="password" placeholder="Password" onChange={passwordChangeHandler}/>
-
-                <input className={classes.loginPassword} type="text" required id="passwordConfirm" placeholder="Confirm Password" onChange={passwordConfirmChangeHandler}/>
+                <input
+                    className={classes.loginText}
+                    type="text"
+                    required id="firstName"
+                    placeholder="First Name"
+                    onChange={firstNameChangeHandler}
+                    value={currentFirstName}
+                />
+                <input
+                    className={classes.loginText}
+                    type="text"
+                    required id="lastName"
+                    placeholder="Last Name"
+                    onChange={lastNameChangeHandler}
+                    value={currentLastName}
+                />
+                <input
+                    className={classes.loginText}
+                    type="text"
+                    required id="email"
+                    placeholder="Email"
+                    onChange={emailChangeHandler}
+                    value={currentEmail}    
+                />
+                <input
+                    className={classes.loginPassword}
+                    type="text"
+                    required id="password"
+                    placeholder="Password"
+                    onChange={passwordChangeHandler}
+                    value={currentPassword}
+                />
+                <input
+                    className={classes.loginPassword}
+                    type="text"
+                    required id="passwordConfirm"
+                    placeholder="Confirm Password"
+                    onChange={passwordConfirmChangeHandler}
+                    value={currentPasswordConfirm}
+                />
             </div>
             <label className={classes.checkboxContainer}>
                 <input className={classes.checkmark} type="checkbox" name="checkbox" value="remember"/>

cee34ffc075f112ef16cce39e6aa727dc620b2a7
jtfreedman <babzibles@gmail.com>
2022-05-28 08:38:38 -0400

    state added to login form - not secure

--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,8 +1,27 @@
+import { useState } from 'react';
 import classes from './LoginForm.module.css'
 
 export default function LoginForm() {
-    function submitHandler(event) {}
-    
+    const [currentEmail, setEmail] = useState('');
+    const [currentPassword, setPassword] = useState('');
+
+    function emailChangeHandler(event) {
+        setEmail(event.target.value);
+    };
+
+    function passwordChangeHandler(event) {
+        setPassword(event.target.value);
+    };
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        //check auth here
+
+        setEmail('');
+        setPassword('');
+    }
+
     return (
         <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.control}>
@@ -11,12 +30,16 @@ export default function LoginForm() {
                     type="text"
                     required id="email"
                     placeholder="Email"
+                    onChange={emailChangeHandler}
+                    value={currentEmail}
                 />
                 <input
                     className={classes.loginPassword}
                     type="text"
                     required id="password"
                     placeholder="Password"
+                    onChange={passwordChangeHandler}
+                    value={currentPassword}
                 />
             </div>
                 <label className={classes.checkboxContainer}>

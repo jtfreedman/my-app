c8ff475a8d81763fdbc65cf4d57f026c98ebe06a
jtfreedman <babzibles@gmail.com>
2022-05-30 18:41:05 -0400

    user page further enchanemnet work

--- src/pages/admin/users/UserList.js
+++ src/pages/admin/users/UserList.js
@@ -1,17 +1,37 @@
-import User from './User';
+//import User from './User';
 import classes from './UserList.module.css';
 
 export default function UserList(props) {
     return (
-        <ul className={classes.list}>
-            {props.users.map(user => 
-            <User 
-                key={user.id} 
-                first={user.firstName}
-                last={user.lastName}
-                email={user.email}
-                password={user.password}
-            />)}
-        </ul>
+        // <ul className={classes.list}>
+        //     {props.users.map(user => 
+        //     <User 
+        //         key={user.id} 
+        //         first={user.firstName}
+        //         last={user.lastName}
+        //         email={user.email}
+        //         password={user.password}
+        //     />)}
+        // </ul>
+        <table className={classes.blueTable}>
+            <thead>
+            <tr>
+                <th>Email</th>
+                <th>Last Name</th>
+                <th>First Name</th>
+            </tr>
+            </thead>
+            <tbody>
+                {
+                    props.users.map((user) => (
+                        <tr key={Math.random(100)}>
+                            <td>{user.email}</td>
+                            <td>{user.firstName}</td>
+                            <td>{user.lastName}</td>
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     );
 }

0966dd0560ed45c382616ddb244439f8f5d00103
jtfreedman <babzibles@gmail.com>
2022-06-04 18:36:25 -0400

    throws error and does not create user if taken

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -6,20 +6,49 @@ import classes from './CreateAccount.module.css';
 
 function CreateAccount() {
     const [showPasswordWarning, setPasswordWarningState] = useState(false);
+    const [showEmailWarning, setEmailWarningState] = useState(false);
+    const [createUser, setCreateUserState] = useState(false);
 
     function addUserHandler(userData) {
         fetch(
             'https://my-app-1b537-default-rtdb.firebaseio.com/users.json',
+        ).then((response) => {
+            return response.json();
+        }).then((data) => {
+            for (const key in data)
             {
-                method: 'POST',
-                body: JSON.stringify(userData),
-                headers: {
-                    'Content-Type': 'application/json'
+                const user = 
+                {
+                    id: key,
+                    ...data[key]
+                };
+
+                if (userData.email === user.email)
+                {
+                    setEmailWarningState(true);
+                }
+                else
+                {
+                    setCreateUserState(true);
                 }
             }
-        ).then(() => {
-            navigate('/', {replace:true});
         });
+
+        if (showEmailWarning === false && createUser)
+        {
+            fetch(
+                'https://my-app-1b537-default-rtdb.firebaseio.com/users.json',
+                {
+                    method: 'POST',
+                    body: JSON.stringify(userData),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            ).then(() => {
+                navigate('/', {replace:true});
+            });
+        }
     }
 
     function passwordFailHandler(userData) {
@@ -36,6 +65,7 @@ function CreateAccount() {
     return (
         <div className={classes.createAccountForm}>
             {showPasswordWarning && <Warning>Passwords do not match.</Warning>}
+            {showEmailWarning && <Warning>Email is already taken.</Warning>}
             <div className={classes.loginButton} onClick={routeChange}>Login</div>
             <div className={classes.createAccount}>Create Account</div>
             <NewUserForm onAddUser={addUserHandler} onPasswordFail={passwordFailHandler}/>
